157b8c0a6dac784739a1822dee8b9e6f9d1a6a8b
PavithMadushan <pavithmadushan@gmail.com>
2024-12-01 20:52:34 +0530

    fn onboarding screens are done

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import { Redirect } from "expo-router";
+import React from "react";
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+const SignIn = () => {
+
+    return(
+        <SafeAreaView>
+            <Text>signin</Text>
+        </SafeAreaView>
+    )
+  
+  
+    // return <Redirect href="/(auth)/welcome"/>;
+  
+};
+
+export default SignIn;
---
+++ app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+import { Redirect } from "expo-router";
+import React from "react";
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+const Home = () => {
+
+    return(
+        <SafeAreaView>
+            <Text>Home</Text>
+        </SafeAreaView>
+    )
+  
+  
+    // return <Redirect href="/(auth)/welcome"/>;
+  
+};
+
+export default Home;
