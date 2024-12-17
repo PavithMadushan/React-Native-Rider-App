0a8f82023115240b76d556518c6cf5d8c3c06de9
PavithMadushan <pavithmadushan@gmail.com>
2024-12-18 00:40:56 +0530

    map card done

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,9 +1,12 @@
+import GoogleTextInput from '@/components/GoogleTextInput';
 import RideCard from '@/components/RideCard';
+import { icons, images } from '@/constants';
 import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
 import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
+import { Text, View, Image, ActivityIndicator, TouchableOpacity } from 'react-native'
 import { FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Map from '@/components/Map';
 
 
 const recentRids= [
@@ -108,13 +111,63 @@ const recentRids= [
 
 export default function Page() {
   const { user } = useUser();
+  const loading=true;
 
+  const handleSignOut=()=>{
+
+
+  }
+
+  const handleDestinationPress=()=>{}
 
 
   return (
     <SafeAreaView className='bg-general-500'>
-      <FlatList data={recentRids.slice(0,5)}
-      renderItem={({item})=><RideCard ride={item}/>}/>
+      <FlatList data={recentRids?.slice(0,5)}
+      renderItem={({item})=><RideCard ride={item}/>}
+      className='px-5'
+      keyboardShouldPersistTaps="handled"
+      contentContainerStyle={{
+        paddingBottom:100
+      }}
+      ListEmptyComponent={()=>(
+        <View className='flex flex-col items-center justify-center'>
+          {!loading?(
+            <>
+            <Image source={images.noResult} className='w-40 h-40' alt='No recent rides found' resizeMode='contain'/>
+            <Text>No recent rides found</Text>
+            </>
+          ):(
+            <>
+            <ActivityIndicator size='small' color='#000'/>
+            </>
+          )}
+        </View>
+      )}
+
+      ListHeaderComponent={()=>(
+        <>
+        <View className='flex flex-row items-center justify-between my-5'>
+          <Text className='text-2xl capitalize font-JakartaExtraBold'>Welcome {", "} {user?.firstName || user?.emailAddresses[0].emailAddress.split('@')[0]}{" "} 👋</Text>
+          <TouchableOpacity onPress={handleSignOut} className='justify-center items-center w-10 h-10 rounded-full bg-white'>
+            <Image source={icons.out} className='w-4 h-4'/>
+          </TouchableOpacity>
+        </View>
+        <GoogleTextInput
+        icon={icons.search}
+        containerStyle='bg-white shadow-md shadow-neutral-300'
+        handlePress={handleDestinationPress}/>
+        <>
+        <Text className='text-xl font-JakartaBold mt-5 mb-3'>Your Current Location</Text>
+        <View className='flex flex-row items-center bg-transparent h-[300px]'>
+          <Map/>
+        </View>
+        </>
+        <Text className='text-xl font-JakartaBold mt-5 mb-3'>Your Recent Rides</Text>
+        </>
+      )}
+      
+      />
       
     </SafeAreaView>
   )
